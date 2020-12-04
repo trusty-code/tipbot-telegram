3bae83b81c5ea5d46b3a30bf87b0d1167ce98d7f
NO8ODY <44928989+no8ody@users.noreply.github.com>
2020-12-04 22:30:53 +0100

    add /del feature

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ const trustify = require('@trustify/tipbot.ts');
 var TinyURL = require('tinyurl');
 const Telegram = require('telegraf/telegram')
 
-
 const BOT_TOKEN = process.env.BOT_TOKEN
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
@@ -17,20 +16,36 @@ const DB_URL = process.env.DB_URL
 const DB_PORT = process.env.DB_PORT
 const DB_NAME = process.env.DB_NAME
 
-
 const bot = new Telegraf(BOT_TOKEN)
 const telegram = new Telegram(BOT_TOKEN, {})
 
 const addaddr = `Address added.`
 const invalidaddr = `Error - Probably not a valid IOTA address`
+const notfound = `User not found!`
 
 trustify.setDB(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_URL}:${DB_PORT}/${DB_NAME}`)
+
 
 bot.start((ctx) =>
    ctx.reply('Hi! I\'m the IOTA TipBot! \r\nType /help to see a list of available commands. First, you need to set a valid donation address with /add <your address>.'))
 
 bot.help((ctx) =>
    ctx.reply('/start - to start using the bot and receive qr-codes \r\n/help - some useful tips to this bot \r\n/add - set your IOTA receiving address \r\n/tip - tip to a User'))
+
+bot.hears(/^!del|^\/del/i, async (ctx) => {
+   let msg = ctx.message.text
+   if (msg === `!del`|| msg === `/del`) {
+      var user = ctx.message.from.username
+      let response = await trustify.del(user)
+      if (response !== notfound) {
+         ctx.reply(response)
+      } else {
+         ctx.reply('User @' + user + ' not found!')
+      }
+   } else {
+      ctx.reply('Please only write /del to remove your tip account!')
+   }
+})
 
 bot.hears(/^!add|^\/add/i, async (ctx) => {
    let user = ctx.message.from.username
@@ -95,6 +110,7 @@ bot.hears(/^!tip|^\/tip/i, async (ctx) => {
 })
 
 bot.action("send_qr_code", (ctx) => {
+
    let message = ctx.update.callback_query.message.text
 
    let address = message.substring(0, 90)
@@ -102,12 +118,14 @@ bot.action("send_qr_code", (ctx) => {
    let username = message.substring(100)
 
    let user_id = ctx.update.callback_query.from.id
-   
+
    telegram.sendPhoto(user_id, `https://api.qrserver.com/v1/create-qr-code/?data=${address}`, {caption: `${tip_text} @${username}`}).then(res =>  {
       console.log("sendMessage", res)
    })
+
    return ctx.answerCbQuery(`Private message sent.`)
 })
+
 
 bot.command('source', (ctx) =>
    ctx.reply(`GitHub:`, Extra.HTML().markup((m) =>
